2949a8d5fe467876587f552bf48d5d23e7e837b4
Philip Joyner <filljoyner@gmail.com>
2018-06-22 16:59:59 -0500

    added header and footer files for test pack

---
+++ _wpm/pack/test/src/resources/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css" integrity="sha384-MIwDKRSSImVFAZCVLtU0LMDdON6KVCrZHyVQQj6e8wIEJkW4tvwqXrbMIya1vriY" crossorigin="anonymous">
+    
+    <script   src="https://code.jquery.com/jquery-3.1.0.min.js"   integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="   crossorigin="anonymous"></script>
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 p-t-1 p-b-1">
+            
+        
